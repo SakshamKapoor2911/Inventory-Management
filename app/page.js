d09b6b019ce21e76f60c297ebbad27ca05b3aab0
SakshamKapoor2911 <124176804+SakshamKapoor2911@users.noreply.github.com>
2024-08-07 11:25:34 +0530

    Your commit message

--- app/page.js
+++ app/page.js
@@ -141,9 +141,14 @@ export default function Home() {
                 <Typography variant={'h3'} color={'#333'} textAlign={'center'}>
                 {quantity}
                 </Typography>
+                <Stack direction="row" spacing={3}>
+                <Button variant="contained" onClick={() => addItem(name)}>
+                  Add
+                </Button>
                 <Button variant="contained" onClick={() => removeItem(name)}>
                   Remove
                 </Button>
+                </Stack>
               </Box>
             ))}
           </Stack>
